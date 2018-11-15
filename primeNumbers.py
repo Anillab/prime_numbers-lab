71406e1d460f47b322988ef0a6b041091fdc1122
Anillab <anillabusolo@gmail.com>
2018-11-15 16:38:53 +0300

    defined a function to generate prime numbers

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,8 @@
-
+def primenumber_generator(x):
+    for x in range(1,x+1):
+        if x>1:
+            for i in range(2,x):
+                if x%i==0:
+                    break
+            else:
+                print(x)
